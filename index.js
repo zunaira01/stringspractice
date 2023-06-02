dbefd569ebc86bc270f6c5fb123c01602176ad2c
zunaira01 <zunaira.mshaukat@gmail.com>
2023-06-02 14:55:28 -0400

    if statement

---
+++ index.js
@@ -0,0 +1,139 @@
+// let x=15;
+// console.log(x);
+
+// let a=7;
+// console.log(a);
+
+let names=["jim", "james", "paul", "tyrone"]
+console.log(names);
+
+let seed1="carrots";
+console.log(seed1);
+
+let vegtables=20;
+console.log(vegtables);
+
+// let x=28;
+// console.log(x);
+
+// let x=12;
+// let a=78;
+
+// if (x ===12 && a ===78) {
+//     let sum=x+a;
+//     console.log(sum);
+// }
+// let b=14
+// let c=98
+
+// if (b ===14 && c ===98) {
+//     let sum=x+a;
+//     console.log(sum);}
+
+let age = 18;
+    if (age>18) {
+    //do this
+    console.log("access granted");
+    }
+    else{
+    //do this
+    console.log("access denied");
+    }
+
+//     let wood = 40;
+//     if(wood<40) {
+// // we have a enough for a tree house
+// console.log("enough");
+//     }
+//     else{
+//      // too bad
+//      console.log("not enough");
+// }
+
+//     let temperature = 70;
+//     if (temperature<70) {
+//         // we can go out to swim
+//     }
+//     else{
+//         // pool will be closed
+//     console.log("too cold")
+// }
+
+let temperature = 100;
+if (temperature >100) {
+    // too hot
+    
+}
+else{
+    // cannot go out
+    console.log("too hot")
+}
+
+
+// let password = 164
+// if (password=164) {
+//     // get in
+//     console.log("access granted");
+// }
+// else{
+//     // cannot get in
+//     console.log("access denied");
+// }
+
+
+let username = 'admin';
+let password = 'pass164';
+
+if (username === 'admin'&& password === 'pass164') {
+    console.log("access granted");
+}
+else{
+    console.log("access denied");
+}
+
+let wood=19;
+if (wood >= 40){
+    // enough for treehouse
+    console.log("build a treehouse")
+}
+    else if (wood >= 20){
+        // too bad
+        console.log("build stool");
+    }
+else{
+    console.log("you are s.o.l");
+}
+
+let num = 201
+if (num >= 100){
+    // greater
+    console.log("positive");
+}
+else if (num <= 0 && num >= 0){
+    // less than
+    console.log("negative");
+}
+else if (num >0 && num >100 ){
+    console.log("positive");
+}
+else{
+    console.log("num is < 0 than num is negative");
+}
+
+let score = 60;
+if (score >= 80){
+    // you did a great job
+    console.log("good job");
+}
+else if (score >= 70) {
+    console.log("keep trying");
+}
+else if (score >= 60) {
+    console.log("ehh");
+}
+else if (score >= 55 ) {
+    console.log("keep trying");
+}
+    else{
+        console.log("bad Grade");
+    }
